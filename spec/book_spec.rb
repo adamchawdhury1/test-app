70347c42c150ad9755483c68129ce8d8846320ff
Adam Chawdhury <adamchawdhury@tamu.edu>
2026-02-02 22:01:36 -0600

    rspec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  it "has seeded books in the test database" do
-    expect(Book.count).to be >= 5
+  before(:all) do
+    # Load seed data once for all tests in this file
+    load Rails.root.join('db/seeds.rb')
+  end
+
+  it "has test books from seed file" do
+    expect(Book.count).to eq(5)
+    expect(Book.first.title).to eq("test 1")
   end
 end
